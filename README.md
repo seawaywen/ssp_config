2aad27e75accd20e478a1be14c4d69061c34aa76
kelvin.li <kelvin.li@canonical.com>
2016-04-18 14:27:58 +0800

    added the missing script and readme files

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# ssp_config
-shadowsocks setup
+# Shadowsocks commandline local client setup (Tested on Ubuntu)
+1. Checkout the code
+2. You need to install the shadowsocks by running `pip install shadowsocks`
+3. Download your shadowsock server config json files to configs folder if you're using vpnso service
+4. Running the script: `python vpnsso_config_converter.py` to convert the vpnso config files to the sslocal format
+and save to the root folder
+If you're not using vpnso service, keep your config file format like following under the project root folder
+with file name like config_xx.json:
+    {
+        "server":"my ip",
+        "server_port":8388,
+        "local_port":1080,
+        "password":"my password",
+        "timeout":600,
+        "method":"aes-256-cfb"
+    }
+
+5. Set the alias like:
+`alias sso='/home/kelvin/DevTools/sso.sh'`
+6. Run the commands: ssp xx to start or switch among the ss servers.
+if you have config_xx.json, you can run `ssp xx`
+and when you're ready to switch to config_yy.json, just run `ssp yy`,
+it will automatically help you kill the exising process and recreate new one.
--- vpnsso_config_converter.py
+++ vpnsso_config_converter.py
@@ -5,7 +5,6 @@ import os
 def get_content_from_json_file(json_file):
     with open(json_file) as jf:
         content = json.load(jf)
-        print content
         local_port = content['local_port']
         server_password_section = content['server_password'][0]
         server_with_port, password, method = server_password_section
@@ -18,31 +17,43 @@ def get_content_from_json_file(json_file):
             "password": password,
             "method": method
         }
-        #print server, port, password, method,
         return new_content
 
 def generate_new_json_file(parent_path, file_name, **content):
     file_path = os.path.join(parent_path, file_name)
-    print file_path
     with open(file_path, 'w') as new_file:
         json.dump(content, new_file)
+    print('saving new config file:{}'.format(file_path))
 
 
 def iter_dir(path):
+    analyze_flag = False
     for parent, dirnames, filenames in os.walk(path):
         for dirname in dirnames:
             print 'parent dir:' + parent
-            #print 'dirname is ' + dirname
 
         for filename in filenames:
+
             #print 'parent is:' + parent
             print 'filename is ' + filename
             file_full_path = os.path.join(parent, filename)
-            print 'full name of file is ' + file_full_path
+            #print 'full name of file is ' + file_full_path
 
-            new_content = get_content_from_json_file(file_full_path)
-            print new_content
-            generate_new_json_file(os.path.dirname(parent), filename, **new_content)
+            if filename.endswith('.json'):
+                new_content = get_content_from_json_file(file_full_path)
+                #print new_content
+                generate_new_json_file(os.path.dirname(parent), filename, **new_content)
+
+                analyze_flag = True
+    return analyze_flag
+
 
 if __name__ == '__main__':
-    iter_dir('/home/kelvin/DevTools/ss/configs')
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    config_dir = os.path.join(base_dir, 'configs')
+    print config_dir
+    if not os.path.exists(config_dir):
+        raise ValueError('you must have a configs folder to contain all the '
+                         'config json files')
+    if not iter_dir(config_dir):
+        print('you don NOT have any json config file')
